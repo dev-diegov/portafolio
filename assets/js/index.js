d5199e5f5b937e1ca25769ea598c958d0374061d
Diego Donoso <die.idonosov@gmail.com>
2021-10-14 20:58:51 -0300

    Arreglo de footer e iconos

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,10 +4,3 @@ const navMenu = document.querySelector(".nav-menu");
 navToggle.addEventListener("click", () => {
   navMenu.classList.toggle("nav-menu_visible");
 });
-
-const btnSwitch = document.querySelector('#switch');
-
-btnSwitch.addEventListener('click', () => {
-  document.body.classList.toggle('dark');
-  btnSwitch.classList.toggle('active');
-});
