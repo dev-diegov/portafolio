888f6fcf17ad83a77ec69f820691c7abd45e9852
Diego Donoso <die.idonosov@gmail.com>
2021-10-14 20:30:47 -0300

    Proyectos linkeables

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,3 +4,10 @@ const navMenu = document.querySelector(".nav-menu");
 navToggle.addEventListener("click", () => {
   navMenu.classList.toggle("nav-menu_visible");
 });
+
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+  btnSwitch.classList.toggle('active');
+});
